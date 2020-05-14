f4549d1352d4ace848e3291e789f757973ecda21
wwg2 <wwg2@njit.edu>
2020-05-14 14:36:23 -0400

    create config.php

---
+++ folder/config.php
@@ -0,0 +1,6 @@
+<?php
+$dbuser = "wwg2";//your ucid
+$dbpass = "Dukeman1997!";//your phpMyAdmin password
+$dbhost = "sql1.njit.edu";//whichever server you login to from web.njit.edu/mysql/phpMyAdmin
+$dbdatabase = "wwg2";//your ucid
+?>
